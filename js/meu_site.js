1fc3b7fc0782f48903ef232b5b24f11a5ec6e8da
Carlos Tadeu de Oliveira <carlos@megasolutions.com.br>
2019-04-10 16:29:34 -0300

    site finalizado

--- js/meu_site.js
+++ js/meu_site.js
@@ -2,11 +2,3 @@ var nome = prompt("Por favor, insira seu nome");
 
 alert("Prazer " + nome + ", seja bem-vindo ao meu site!");
 
-function turnOn(){
-
-	var texto = document.getElementById("texto_neon");
-	texto.className = texto.className.concat("on");
-
-	var imagem = document.getElementById("img_neon");
-	imagem.setAttribute("class", imagem.getAttribute("class").concat("on"));
-}
