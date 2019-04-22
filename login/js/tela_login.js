c0dffd1ec6ff6a25c07736b6c00f4618c0ef4b62
matheus-aono <49319401+matheus-aono@users.noreply.github.com>
2019-04-22 16:26:42 -0300

    Add files via upload

--- login/js/tela_login.js
+++ login/js/tela_login.js
@@ -1,1 +1,16 @@
-alert("Teste");
+$(document).ready(function(){
+
+  $("#formulario").on("submit", function(e){
+          e.preventDefault();
+          var len1 = $("#nome").val().length;
+          var len2 = $("#sobrenome").val().length;
+          if (len1 > 3 && len2 > 3) {
+              localStorage.setItem("nome", $("#nome").val());
+              localStorage.setItem("sobrenome", $("#sobrenome").val());
+              this.submit();
+          }else{
+            alert("Nome e sobrenome devem conter no minimo 3 caracteres!");
+          }
+      });
+});
+
