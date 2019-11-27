0060c983393760c90b92974db5d408877d1e350c
matheus-aono <aono.1304@gmail.com>
2019-11-27 16:57:10 -0300

    finalizando javascript-alura 27/11/19

--- javascript-alura/js/busca-paciente.js
+++ javascript-alura/js/busca-paciente.js
@@ -4,10 +4,24 @@ buscar.addEventListener("click", function(){
     
     var api = new XMLHttpRequest();
 
-    api.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
+    api.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     api.addEventListener("load", function(){
-        console.log(api.responseText);
+        
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if(api.status == 200){
+            erroAjax.classList.add("invisivel");
+            var resposta = api.responseText;
+            var paciente = JSON.parse(resposta);
+
+            paciente.forEach(function(paciente) {
+            adicionaPacienteNaTabela(paciente);
+        });
+        } else {
+            erroAjax.classList.remove("invisivel");
+        }
+
         
     });
 
