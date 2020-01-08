71d3a9fb34beaf8c30afa29c590413a05cc0f277
matheus-aono <49319401+matheus-aono@users.noreply.github.com>
2020-01-08 05:10:53 -0300

    Add files via upload

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,30 +1,25 @@
-class NegociacaoController{
-
-    constructor(){
-        let $ = document.querySelector.bind(document);
-
-        this._inputData = $("#data");
-        this._inputQuantidade = $("#quantidade");
-        this._inputValor = $("#valor");
-    }
-
-    adiciona(event){
-        event.preventDefault();
+class NegociacaoController {
+    
+    constructor() {
         
-        let data = new Date(...this._inputData.value.split("-").map(function(item, indice){
-            if(indice == 1){
-                return item - 1;
-            } else {
-                return item;
-            }
-        }));
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+    
+    adiciona(event) {
+        
+        event.preventDefault();
 
-        let negociacao = new NegociacaoController(
-            data,
-            this._inputQuantidade,
-            this._inputValor
-        )
+        let negociacao = new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value  
+        );
+
         console.log(negociacao);
+        console.log(DateHelper.dataParaTexto(negociacao.data));
         
     }
 }
